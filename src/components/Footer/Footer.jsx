00b67d2d885b81de3ed1f64c09204a85da063cd2
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-24 10:41:58 +0100

    Ajout du footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react"
+import Logo from "../Logo/Logo"
 
 export default function Footer() {
-    return <div>Footer</div>
+    return (
+        <footer>
+            <Logo />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
 }
