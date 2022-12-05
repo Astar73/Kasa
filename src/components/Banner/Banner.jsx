e18ce4a73194077455780e6e7738098a18d1dce9
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:46:03 +0100

    Modif composant Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,8 @@
-import React from "react"
+import React from 'react'
 
 export default function Banner({ image, title }) {
     return (
-        <div className="banner">
-            <img className="banner__img" src={image} alt="Bannière" />
+        <div className="banner" style={{ backgroundImage: `url(${image})` }}>
             <h1 className="banner__title">{title}</h1>
         </div>
     )
