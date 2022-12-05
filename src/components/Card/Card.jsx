ffd4d0f57f4da306e54310f249580fd7fbdeb8fb
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:41:00 +0100

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import products from "../../data/logements.json"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import products from '../../data/logements.json'
 
 export default function Card() {
     return (
@@ -9,11 +9,7 @@ export default function Card() {
                 return (
                     <article className="card" key={product.id}>
                         <Link to={`/products/${product.id}`}>
-                            <img
-                                src={product.cover}
-                                alt="Logement"
-                                className="card__img"
-                            />
+                            <img src={product.cover} alt="Logement" className="card__img" />
                             <p className="card__title">{product.title}</p>
                         </Link>
                     </article>
