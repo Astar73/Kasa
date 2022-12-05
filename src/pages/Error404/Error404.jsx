fe3898074e35ad09efe7128cbf3b80eefe99c8d4
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:43:57 +0100

    Update Error404.jsx

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,17 +1,13 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Error404() {
     return (
         <main className="error">
             <h1 className="error__title">404</h1>
-            <p className="error__text">
-                Oups! La page que vous demandez n'existe pas.
-            </p>
+            <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
             <Link to="/">
-                <p className="error__text-link">
-                    Retourner sur la page d’accueil
-                </p>
+                <p className="error__text-link">Retourner sur la page d’accueil</p>
             </Link>
         </main>
     )
