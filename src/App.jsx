f5fa4824edccbc66d4c3a90095535a38e3cd0054
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:37:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import Header from "./components/Header/Header.jsx"
-import Home from "./pages/Home/Home.jsx"
-import FichesLogement from "./pages/FichesLogement/FichesLogement.jsx"
-import Error404 from "./pages/Error404/Error404.jsx"
-import About from "./pages/About/About.jsx"
-import Footer from "./components/Footer/Footer.jsx"
-import "./style/main.css"
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header.jsx'
+import Home from './pages/Home/Home.jsx'
+import FichesLogement from './pages/FichesLogement/FichesLogement.jsx'
+import Error404 from './pages/Error404/Error404.jsx'
+import About from './pages/About/About.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import './style/main.css'
 
 function App() {
     return (
