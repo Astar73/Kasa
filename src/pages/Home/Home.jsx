485699381c9aa445f38eb18d403f72a1326db945
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:43:40 +0100

    Changement du nom de l'import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import Banner from "../../components/Banner/Banner"
-import image from "../../images/Banner.png"
-import Card from "../../components/Card/Card"
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+import Card from '../../components/Card/Card'
+import BannerImg from '../../images/Banner.png'
 
 export default function Home() {
     return (
         <main className="home">
-            <Banner image={image} title="Chez vous, partout et ailleurs" />
+            <Banner image={BannerImg} title="Chez vous, partout et ailleurs" />
             <Card />
         </main>
     )
