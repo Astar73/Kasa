8bf11e88e900434b2b3f1d1c9ecf3358414a50e0
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-05 11:46:38 +0100

    Update FichesLogement.jsx

--- src/pages/FichesLogement/FichesLogement.jsx
+++ src/pages/FichesLogement/FichesLogement.jsx
@@ -1,26 +1,30 @@
-import React from "react"
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import logements from '../../data/logements.json'
+import aboutDropdown from '../../data/about-dropdown.json'
+import Carousel from '../../components/Carousel/Carousel'
+import Dropdown from '../../components/Dropdown/Dropdown'
 
 export default function FichesLogement() {
+    const { id } = useParams()
+    const logement = logements.find((logement) => logement.id === id)
+    const { title, pictures, description, host, rating, location } = logement
+
     return (
-        <div className="logement">
-            <Carousel />
+        <main className="logement">
+            <Carousel slides={pictures} />
             <div className="logement__content">
                 <div className="logement__header">
-                    <h1 className="logement__title"></h1>
-                    <p className="logement__location"></p>
-                    <div className="logement__tags">
-                        <Tags />
-                    </div>
+                    <h1 className="logement__title">{title}</h1>
+                    <p className="logement__location">{location}</p>
+                    <div className="logement__tags"></div>
                 </div>
-                <div className="logement__host-rate">
-                    <Host />
-                    <Rating />
-                </div>
+                <div className="logement__host-rate"></div>
             </div>
-            <div className="logement__collapse">
-                <Collapse />
-                <Collapse />
+            <div className="logement__dropdown">
+                <Dropdown />
+                <Dropdown />
             </div>
-        </div>
+        </main>
     )
 }
