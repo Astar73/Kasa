d347da56b90d71f8557bc98bcb08be006b41a2af
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-13 10:28:34 +0100

    Update Dropdown.jsx
    Modif img, valeur usestate et ajout d'une  liste à puce

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
-import topArrow from '../../images/TopArrow.svg'
+import arrow from '../../images/TopArrow.svg'
 
-export default function Dropdown({ title, content }) {
-    const [active, setActive] = useState(false)
+export default function Dropdown({ title, content, isList }) {
+    const [active, setActive] = useState(true)
 
     const activeDropdown = () => {
         setActive(!active)
@@ -14,10 +14,20 @@ export default function Dropdown({ title, content }) {
             <div className="dropdown__title" onClick={activeDropdown}>
                 {title}
                 <span className="dropdown__icon">
-                    <img src={topArrow} alt="Flèche vers le haut" />
+                    <img src={arrow} alt="Flèche vers le haut" />
                 </span>
             </div>
-            <div className="dropdown__content">{content}</div>
+            <div className="dropdown__content">
+                {isList ? (
+                    <ul>
+                        {content.map((value) => (
+                            <li>{value}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    content
+                )}
+            </div>
         </div>
     )
 }
