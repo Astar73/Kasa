4c1455ff8a7c8b82c97138f9cb8a85c610682b6f
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-13 10:35:47 +0100

    Ajout des menus déroulants

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
 import BannerImg from '../../images/AboutBanner.png'
+import aboutDropdown from '../../data/about-dropdown.json'
+import Dropdown from '../../components/Dropdown/Dropdown'
 
 export default function About() {
     return (
-        <main>
+        <main className="about">
             <Banner image={BannerImg} title="" />
+            {aboutDropdown.map((text) => {
+                return <Dropdown title={text.title} content={text.content} />
+            })}
         </main>
     )
 }
