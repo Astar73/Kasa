d1abfc446f8179adcf8b531156ad82c8b8aa6d42
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-12-13 10:23:58 +0100

    Update Banner.jsx
    Ajout d'un div pour gérer la banner mobile différente

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Banner({ image, title }) {
     return (
         <>
-            <div className="banner filter" style={{ backgroundImage: `url(${image})` }}></div>
+            <div className="banner filter" style={{ background: `url(${image})` }}></div>
+            <div className="banner--mobile filter"></div>
             <h1 className="banner__title">{title}</h1>
         </>
     )
